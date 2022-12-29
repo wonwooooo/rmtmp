7131b628982e4871efc3669c112cab6da6c22a86
wonwooooo <codetralopithecus@gmail.com>
2022-12-30 04:21:13 +0900

    reset

--- rmsoft/models.py
+++ rmsoft/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    company_name = models.CharField(max_length=200, primary_key=True)
+    company_name = models.CharField(max_length=200)
     company_boss_name = models.CharField(max_length=200)
     company_phone_number = models.CharField(max_length=200)
 
