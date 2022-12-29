50b2eef762f452ef4c97e1066e0bbb638e3c9af4
wonwooooo <codetralopithecus@gmail.com>
2022-12-29 18:39:16 +0900

    test
    test

--- rmsoft/admin.py
+++ rmsoft/admin.py
@@ -3,3 +3,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Company, Client, Product, Order
+
+
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
