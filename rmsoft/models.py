e06a8a46e9324481f1640e9f088bb57f6629c733
wonwooooo <codetralopithecus@gmail.com>
2022-12-30 04:32:19 +0900

    Update models.py

--- rmsoft/models.py
+++ rmsoft/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     p_name = models.CharField(max_length=200)
     p_price = models.IntegerField()
     p_registerdate = models.DateTimeField(auto_now_add=True)
-    p_company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
+    p_company_name = models.ForeignKey(Company, to_field='company_name', on_delete=models.CASCADE)
 
 class Client(models.Model):
     client_name = models.CharField(max_length=200)
