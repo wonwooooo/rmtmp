1df3e4224e1387718d548aecd37c6ce718aabc87
wonwooooo <codetralopithecus@gmail.com>
2022-12-30 05:04:10 +0900

    Update models.py

--- rmsoft/models.py
+++ rmsoft/models.py
@@ -3,15 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    company_name = models.CharField(max_length=200, unique=True)
+    company_name = models.CharField(max_length=200)
     company_boss_name = models.CharField(max_length=200)
     company_phone_number = models.CharField(max_length=200)
+
+    def natural_key(self):
+        return self.company_name
 
 class Product(models.Model):
     p_name = models.CharField(max_length=200)
     p_price = models.IntegerField()
     p_registerdate = models.DateTimeField(auto_now_add=True)
-    p_company_name = models.ForeignKey('Company', to_field='company_name', on_delete=models.CASCADE, db_column="p_company_name")
+    p_company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 class Client(models.Model):
     client_name = models.CharField(max_length=200)
